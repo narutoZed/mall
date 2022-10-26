a2d869407905e90f826088848f7bf4b0c5001a06
unknown <674427539@qq.com>
2022-10-26 15:54:12 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+<a target="_blank" href="https://www.bilibili.com/video/BV15741177Eh?p=148&spm_id_from=pageDriver&vd_source=d4154056fc66f8f5c5ceabdb70407a6e">新建窗口打开超链接</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
